4bd43d0cc0773b3fb3f78614f418fb6b33851eff
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-09-06 02:49:08 +0900

    More fix

--- crates/turbopack-ecmascript-plugins/src/transform/directives/client.rs
+++ crates/turbopack-ecmascript-plugins/src/transform/directives/client.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use async_trait::async_trait;
-use swc_core::base::resolver;
 use swc_ecma_ast::Program;
+use swc_ecma_transforms_base::resolver;
 use swc_ecma_visit::VisitMutWith;
 use turbo_tasks::Vc;
 use turbopack_ecmascript::{CustomTransformer, TransformContext};
