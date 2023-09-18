29c46aa7c1f5ba020fec027d90e0ae6c44d51875
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-19 05:52:10 +0900

    Fix imports

--- crates/turbopack-ecmascript/src/references/pattern_mapping.rs
+++ crates/turbopack-ecmascript/src/references/pattern_mapping.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use indexmap::IndexMap;
-use swc_ecma_ast::Expr;
+use swc_core::quote;
+use swc_ecma_ast::{Expr, Lit};
 use turbo_tasks::{debug::ValueDebug, Value, Vc};
 use turbopack_core::{
     chunk::{
